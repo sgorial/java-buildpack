7b3d35a00e2cbd71759227850eb17bfa8f3d0976
Simon Gorial <simon.gorial@gmail.com>
2014-12-01 13:27:49 -0500

    Update buildpack.rb

--- lib/java_buildpack/buildpack.rb
+++ lib/java_buildpack/buildpack.rb
@@ -61,7 +61,6 @@ module JavaBuildpack
       fail 'No container can run this application' unless container
 
       component_detection('JRE', @jres, true).first.compile
-      component_detection('SERVER', @servers, true).first.compile
       component_detection('framework', @frameworks, false).each(&:compile)
       container.compile
     end
@@ -75,7 +74,6 @@ module JavaBuildpack
       fail 'No container can run this application' unless container
 
       component_detection('JRE', @jres, true).first.release
-      component_detection('SERVER', @servers, true).first.release
       component_detection('framework', @frameworks, false).each(&:release)
       command = container.release
 
@@ -114,8 +112,6 @@ module JavaBuildpack
       components = JavaBuildpack::Util::ConfigurationUtils.load 'components'
 
       @jres       = instantiate(components['jres'], additional_libraries, application, mutable_java_home, java_opts,
-                                app_dir)
-      @servers       = instantiate(components['server'], additional_libraries, application, mutable_java_home, java_opts,
                                 app_dir)
       @frameworks = instantiate(components['frameworks'], additional_libraries, application, immutable_java_home,
                                 java_opts, app_dir)
