438b07baadd5fb72876c00e1a865a1dcd96474e9
Simon Gorial <simon.gorial@gmail.com>
2014-12-01 11:04:10 -0500

    Update apachehttpd.rb

--- lib/java_buildpack/server/apachehttpd.rb
+++ lib/java_buildpack/server/apachehttpd.rb
@@ -1,29 +1,12 @@
-# Encoding: utf-8
-# Cloud Foundry Java Buildpack
-# Copyright 2013 the original author or authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
 require 'fileutils'
 require 'java_buildpack/component/versioned_dependency_component'
-require 'java_buildpack/jre'
-require 'java_buildpack/jre/memory/openjdk_memory_heuristic_factory'
+require 'java_buildpack/server'
 
 module JavaBuildpack
-  module Jre
+  module Server
 
-    # Encapsulates the detect, compile, and release functionality for selecting an OpenJDK-like JRE.
-    class OpenJDKLike < JavaBuildpack::Component::VersionedDependencyComponent
+    # Encapsulates the detect, compile, and release functionality for selecting an ApacheHTTPD server.
+    class ApacheHTTPD < JavaBuildpack::Component::VersionedDependencyComponent
 
       # Creates an instance
       #
@@ -54,26 +37,13 @@ module JavaBuildpack
       def release
         @droplet.java_opts
         .add_system_property('java.io.tmpdir', '$TMPDIR')
-        .add_option('-XX:OnOutOfMemoryError', killjava)
-        .concat memory
+        .add_option('-XX:OnOutOfMemoryError', killapache)
       end
 
       private
 
-      KEY_MEMORY_HEURISTICS = 'memory_heuristics'.freeze
-
-      KEY_MEMORY_SIZES = 'memory_sizes'.freeze
-
-      private_constant :KEY_MEMORY_HEURISTICS, :KEY_MEMORY_SIZES
-
-      def killjava
-        @droplet.sandbox + 'bin/killjava.sh'
-      end
-
-      def memory
-        sizes      = @configuration[KEY_MEMORY_SIZES] || {}
-        heuristics = @configuration[KEY_MEMORY_HEURISTICS] || {}
-        OpenJDKMemoryHeuristicFactory.create_memory_heuristic(sizes, heuristics, @version).resolve
+      def killapache
+        @droplet.sandbox + 'bin/apachectl.sh stop'
       end
 
     end
