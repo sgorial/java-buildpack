6f7ded7ef81022827a7a28c1210cf8aa0bf630be
Simon Gorial <simon.gorial@gmail.com>
2014-12-01 14:11:13 -0500

    Update apache_httpd.rb

--- lib/java_buildpack/framework/apache_httpd.rb
+++ lib/java_buildpack/framework/apache_httpd.rb
@@ -1,3 +1,19 @@
+# Encoding: utf-8
+# Cloud Foundry Java Buildpack
+# Copyright 2013 the original author or authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 require 'fileutils'
 require 'java_buildpack/component/versioned_dependency_component'
 require 'java_buildpack/framework'
@@ -5,45 +21,50 @@ require 'java_buildpack/framework'
 module JavaBuildpack
   module Framework
 
-    # Encapsulates the detect, compile, and release functionality for selecting an ApacheHTTPD server.
+    # Encapsulates the functionality for enabling zero-touch New Relic support.
     class ApacheHTTPD < JavaBuildpack::Component::VersionedDependencyComponent
-
-      # Creates an instance
-      #
-      # @param [Hash] context a collection of utilities used the component
-      def initialize(context)
-        @application    = context[:application]
-        @component_name = self.class.to_s.space_case
-        @configuration  = context[:configuration]
-        @droplet        = context[:droplet]
-
-        @droplet.java_home.root = @droplet.sandbox
-      end
-
-      # (see JavaBuildpack::Component::BaseComponent#detect)
-      def detect
-        @version, @uri = JavaBuildpack::Repository::ConfiguredItem.find_item(@component_name, @configuration)
-        @droplet.java_home.version = @version
-        super
-      end
 
       # (see JavaBuildpack::Component::BaseComponent#compile)
       def compile
-        download_tar
+        FileUtils.mkdir_p logs_dir
+        download_jar
         @droplet.copy_resources
       end
 
       # (see JavaBuildpack::Component::BaseComponent#release)
       def release
         @droplet.java_opts
-        .add_system_property('java.io.tmpdir', '$TMPDIR')
-        .add_option('-XX:OnOutOfMemoryError', killapache)
+        .add_javaagent(@droplet.sandbox + jar_name)
+        .add_system_property('newrelic.home', @droplet.sandbox)
+        .add_system_property('newrelic.config.license_key', license_key)
+        .add_system_property('newrelic.config.app_name', "'#{application_name}'")
+        .add_system_property('newrelic.config.log_file_path', logs_dir)
+        @droplet.java_opts.add_system_property('newrelic.enable.java.8', 'true') if @droplet.java_home.version[1] == '8'
+      end
+
+      protected
+
+      # (see JavaBuildpack::Component::VersionedDependencyComponent#supports?)
+      def supports?
+        @application.services.one_service? FILTER, 'licenseKey'
       end
 
       private
 
-      def killapache
-        @droplet.sandbox + 'bin/apachectl.sh stop'
+      FILTER = /newrelic/.freeze
+
+      private_constant :FILTER
+
+      def application_name
+        @application.details['application_name']
+      end
+
+      def license_key
+        @application.services.find_service(FILTER)['credentials']['licenseKey']
+      end
+
+      def logs_dir
+        @droplet.sandbox + 'logs'
       end
 
     end
