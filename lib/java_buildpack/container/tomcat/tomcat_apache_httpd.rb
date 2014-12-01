c8de60e77c7801b9945d5d5753196a30ba3caa2d
Simon Gorial <simon.gorial@gmail.com>
2014-12-01 15:12:56 -0500

    Update tomcat_apache_httpd.rb

--- lib/java_buildpack/container/tomcat/tomcat_apache_httpd.rb
+++ lib/java_buildpack/container/tomcat/tomcat_apache_httpd.rb
@@ -1,61 +1,37 @@
-# Encoding: utf-8
-# Cloud Foundry Java Buildpack
-# Copyright 2013 the original author or authors.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-require 'fileutils'
 require 'java_buildpack/component/versioned_dependency_component'
-require 'java_buildpack/container'
 
 module JavaBuildpack
   module Container
 
-    # Encapsulates the functionality for enabling zero-touch New Relic support.
-    class TomcatApacheHttpd < JavaBuildpack::Component::VersionedDependencyComponent
-
-      # Creates an instance
-      #
-      # @param [Hash] context a collection of utilities used the component
-      def initialize(context)
-        super(context) { |candidate_version| candidate_version.check_size(3) }
-      end
+    # Encapsulates the detect, compile, and release functionality for Tomcat logging support.
+    class TomcatLoggingSupport < JavaBuildpack::Component::VersionedDependencyComponent
 
       # (see JavaBuildpack::Component::BaseComponent#compile)
       def compile
-        download(@version, @uri) { |file| expand file }
+        download_jar(jar_name, endorsed)
       end
 
       # (see JavaBuildpack::Component::BaseComponent#release)
       def release
       end
-      
+
+      protected
+
       # (see JavaBuildpack::Component::VersionedDependencyComponent#supports?)
       def supports?
         true
       end
-      
-      def expand(file)
-        with_timing "Expanding Apache to #{@droplet.sandbox.relative_path_from(@droplet.root)}" do
-          FileUtils.mkdir_p @droplet.sandbox
-          shell "tar xzf #{file.path} -C #{@droplet.sandbox} --strip 1 --exclude webapps 2>&1"
 
-          @droplet.copy_resources
-          configure_linking
-          configure_jasper
-        end
+      private
+
+      def endorsed
+        @droplet.sandbox + 'endorsed'
       end
-      
+
+      def jar_name
+        "tomcat_logging_support-#{@version}.jar"
+      end
+
     end
 
   end
