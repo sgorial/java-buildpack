262a2a5063f25bfe4873c157bb4f0debafbbbf7c
Simon Gorial <simon.gorial@gmail.com>
2014-12-01 13:45:53 -0500

    Create apachehttpd.rb

---
+++ lib/java_buildpack/jre/apachehttpd.rb
@@ -0,0 +1,52 @@
+require 'fileutils'
+require 'java_buildpack/component/versioned_dependency_component'
+require 'java_buildpack/server'
+
+module JavaBuildpack
+  module Jre
+
+    # Encapsulates the detect, compile, and release functionality for selecting an ApacheHTTPD server.
+    class ApacheHTTPD < JavaBuildpack::Component::VersionedDependencyComponent
+
+      # Creates an instance
+      #
+      # @param [Hash] context a collection of utilities used the component
+      def initialize(context)
+        @application    = context[:application]
+        @component_name = self.class.to_s.space_case
+        @configuration  = context[:configuration]
+        @droplet        = context[:droplet]
+
+        @droplet.java_home.root = @droplet.sandbox
+      end
+
+      # (see JavaBuildpack::Component::BaseComponent#detect)
+      def detect
+        @version, @uri = JavaBuildpack::Repository::ConfiguredItem.find_item(@component_name, @configuration)
+        @droplet.java_home.version = @version
+        super
+      end
+
+      # (see JavaBuildpack::Component::BaseComponent#compile)
+      def compile
+        download_tar
+        @droplet.copy_resources
+      end
+
+      # (see JavaBuildpack::Component::BaseComponent#release)
+      def release
+        @droplet.java_opts
+        .add_system_property('java.io.tmpdir', '$TMPDIR')
+        .add_option('-XX:OnOutOfMemoryError', killapache)
+      end
+
+      private
+
+      def killapache
+        @droplet.sandbox + 'bin/apachectl.sh stop'
+      end
+
+    end
+
+  end
+end
