b8d0e7a9c00ef320c67d79669f7eb3005433fb7c
Simon Gorial <simon.gorial@gmail.com>
2014-12-02 18:14:59 -0500

    Create apache_server.rb

---
+++ lib/java_buildpack/framework/apache_server.rb
@@ -0,0 +1,42 @@
+require 'java_buildpack/component/modular_component'
+require 'java_buildpack/framework'
+require 'java_buildpack/framework/apache'
+
+module JavaBuildpack
+  module Framework
+
+    # Encapsulates the detect, compile, and release functionality for Tomcat applications.
+    class ApacheServer < JavaBuildpack::Component::ModularComponent
+
+      protected
+
+      # (see JavaBuildpack::Component::ModularComponent#command)
+      def command
+        @droplet.java_opts.add_system_property 'http.port', '$PORT'
+
+        [
+          @droplet.java_home.as_env_var,
+          @droplet.java_opts.as_env_var,
+          "$PWD/#{(@droplet.sandbox + 'server/bin/apachectl start').relative_path_from(@droplet.root)}",
+          'run'
+        ].flatten.compact.join(' ')
+      end
+
+      # (see JavaBuildpack::Component::ModularComponent#sub_components)
+      def sub_components(context)
+        [
+          Apache.new(sub_configuration_context(context, 'apache'))
+        ]
+      end
+
+      # (see JavaBuildpack::Component::ModularComponent#supports?)
+      def supports?
+        true
+      end
+
+      private
+
+    end
+
+  end
+end
