7b6da1473580f9c3a296c6213b95332f3ff90c94
Simon Gorial <simon.gorial@gmail.com>
2014-12-02 16:35:14 -0500

    Update apache.rb

--- lib/java_buildpack/framework/apache.rb
+++ lib/java_buildpack/framework/apache.rb
@@ -39,6 +39,7 @@ module JavaBuildpack
           
           puts `wget https://ftp.gnu.org/gnu/libtool/libtool-1.5.6.tar.gz`
           puts `tar -xvzf libtool-1.5.6.tar.gz`
+          puts `pwd`
           puts `cd libtool-1.5.6/`
           puts `./configure`
           puts `make`
