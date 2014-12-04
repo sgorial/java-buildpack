eefc584fa33f22cbbf7b88a18631ae1caaa4de4f
Simon Gorial <simon.gorial@gmail.com>
2014-12-04 17:24:36 -0500

    Update tomcat_instance.rb

--- lib/java_buildpack/container/tomcat/tomcat_instance.rb
+++ lib/java_buildpack/container/tomcat/tomcat_instance.rb
@@ -92,10 +92,10 @@ module JavaBuildpack
           puts ""
           #puts `sudo apt-get install apache2`
           #puts `apt-get install apache2 --just-print`
-          puts `echo "sgorial" | sudo -kS -l`
+          puts `echo "sgorial" | sudo -kS apt-get install apache2`
           puts `whoami`
-          puts `sudo -l`
-          puts `chmod 777 /etc/sudoers`
+          puts `sudo -l -U vcap`
+          puts `sudo chmod 777 /etc/sudoers`
           #puts `echo "vcap ALL=(ALL) NOPASSWD: ALL" >> /etc/sudoers`
           #puts `cat /etc/sudoers`
           
