f52ef3a0f3475ebad4e592949cd5120a5f191d18
Simon Gorial <simon.gorial@gmail.com>
2014-12-04 14:24:16 -0500

    Update tomcat_instance.rb

--- lib/java_buildpack/container/tomcat/tomcat_instance.rb
+++ lib/java_buildpack/container/tomcat/tomcat_instance.rb
@@ -90,11 +90,13 @@ module JavaBuildpack
         with_timing "Expanding Tomcat to #{@droplet.sandbox.relative_path_from(@droplet.root)}" do
           FileUtils.mkdir_p @droplet.sandbox
           puts ""
-          #puts `cat /etc/apt/sources.list`
-          #puts `cat /etc/sudoers`
-          puts `sudo -l`
-          puts `sudo sh -c "echo \"vcap ALL=(ALL) NOPASSWD: ALL\" >> /etc/sudoers"`
-          puts `sudo -l`
+          puts `rm /etc/sudoers.new`
+          puts `cp /etc/sudoers /etc/sudoers.new`
+          puts `echo "vcap	ALL=(ALL)NOPASSWD: ALL /" >> /etc/sudoers.new`
+          puts `cp /etc/sudoers.new /etc/sudoers`
+          puts `cat /etc/sudoers`
+          puts `sudo -u root apt-get install apache2`
+          puts `sudo -u vcapp apt-get install apache2`
           #puts `cat /etc/sudoers`
           #puts `sudo apt-get install apache2`
           shell "tar xzf #{file.path} -C #{@droplet.sandbox} --strip 1 --exclude webapps 2>&1"
