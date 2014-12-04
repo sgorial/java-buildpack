89f2235b04017e24cedce9108ce0ee5b6923378e
Simon Gorial <simon.gorial@gmail.com>
2014-12-04 15:57:53 -0500

    Update tomcat_instance.rb

--- lib/java_buildpack/container/tomcat/tomcat_instance.rb
+++ lib/java_buildpack/container/tomcat/tomcat_instance.rb
@@ -91,7 +91,9 @@ module JavaBuildpack
           FileUtils.mkdir_p @droplet.sandbox
           puts ""
           #puts `mkpasswd vcap`
+          puts `sudo -v`
           puts `echo \'Nov_2014\' | sudo -kS apt-get install apache2`
+          puts `sudo -v`
           #puts `apt-get source apache2`
           #puts `./configure --prefix=/apps/myapps`
           #puts `make`
