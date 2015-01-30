4b7319bc492481e88f882cbc6fb5a772378a41b7
Simon Gorial <simon.gorial@gmail.com>
2015-01-30 14:10:21 -0500

    Update apache.rb

--- lib/java_buildpack/framework/apache.rb
+++ lib/java_buildpack/framework/apache.rb
@@ -28,37 +28,43 @@ module JavaBuildpack
 
       def expand(file)
         with_timing "Expanding Apache to #{@droplet.sandbox.relative_path_from(@droplet.root)}" do
-          FileUtils.mkdir_p @droplet.sandbox + 'source'
+          #FileUtils.mkdir_p @droplet.sandbox + 'source'
           FileUtils.mkdir_p @droplet.sandbox + 'apache'
-          FileUtils.mkdir_p @droplet.sandbox + 'pcre'
-          shell "tar xzf #{file.path} -C #{@droplet.sandbox}/source --strip 1 2>&1"
+          #FileUtils.mkdir_p @droplet.sandbox + 'pcre'
+          #shell "tar xzf #{file.path} -C #{@droplet.sandbox}/source --strip 1 2>&1"
           
           cd(@droplet.sandbox)
           
-          puts `wget https://ftp.gnu.org/gnu/libtool/libtool-1.5.6.tar.gz`
-          puts `tar -xvzf libtool-1.5.6.tar.gz`
-          cd(@droplet.sandbox + 'libtool-1.5.6')
-          puts `./configure`
-          puts `make`
-          puts `make install`
+          #puts `wget https://ftp.gnu.org/gnu/libtool/libtool-1.5.6.tar.gz`
+          #puts `tar -xvzf libtool-1.5.6.tar.gz`
+          #cd(@droplet.sandbox + 'libtool-1.5.6')
+          #puts `./configure`
+          #puts `make`
+          #puts `make install`
           
-          cd(@droplet.sandbox)
+          #cd(@droplet.sandbox)
 
-          puts `wget http://sourceforge.net/projects/pcre/files/pcre/8.36/pcre-8.36.tar.gz`
-          puts `tar -xvzf pcre-8.36.tar.gz`
-          cd(@droplet.sandbox + 'pcre-8.36')
-          puts `./configure --prefix=#{@droplet.sandbox}/pcre`
-          puts `make`
-          puts `make install`
+          #puts `wget http://sourceforge.net/projects/pcre/files/pcre/8.36/pcre-8.36.tar.gz`
+          #puts `tar -xvzf pcre-8.36.tar.gz`
+          #cd(@droplet.sandbox + 'pcre-8.36')
+          #puts `./configure --prefix=#{@droplet.sandbox}/pcre`
+          #puts `make`
+          #puts `make install`
           
           # Move back to soure root directory for make install
-          cd(@droplet.sandbox + 'source')
+          #cd(@droplet.sandbox + 'source')
 
           # Install core libraries via make utility
           #puts `./configure --prefix=#{@droplet.sandbox}/apache --with-apr=/usr/local/apr-httpd/ --with-apr-util=/usr/local/apr-util-httpd/`
-          puts `./configure --prefix=#{@droplet.sandbox}/apache --with-included-apr --with-pcre=#{@droplet.sandbox}/pcre/bin/pcre-config`
+          #puts `./configure --prefix=#{@droplet.sandbox}/apache --with-included-apr --with-pcre=#{@droplet.sandbox}/pcre/bin/pcre-config`
+          #puts `make`
+          #puts `make install`
+          puts `curl --max-time 180 --location "https://s3.amazonaws.com/covisintrnd.com-software/httpd-2.2.29.tar.gz" | tar xz`
+          puts `cd httpd-2.2.29`
+          puts `./configure --prefix=/app/apache --enable-mods-shared=all --enable-http --enable-deflate --enable-expires --enable-slotmem-shm --enable-headers --enable-rewrite --enable-proxy --enable-proxy-balancer --enable-proxy-http --enable-proxy-fcgi --enable-mime-magic --enable-log-debug --enable-so --with-expat=builtin --with-mpm=event --with-included-apr`
           puts `make`
           puts `make install`
+          puts `chmod -R uog+rx #{@droplet.sandbox}/apache`
           puts `touch #{@droplet.sandbox}/apache/logs/access_log`
           puts `touch #{@droplet.sandbox}/apache/logs/error_log`
           
@@ -67,6 +73,8 @@ module JavaBuildpack
           
           # Search and replace Listen port with VCAP_PORT variable
           puts `sed -i \'s/VCAP_PORT/#{$PORT}/g\' #{@droplet.sandbox}/apache/conf/httpd.conf`
+          
+          puts `#{@droplet.sandbox}/apache/conf/httpd.conf`
           
           # Finally bring up Apache server
           puts `sh -x #{@droplet.sandbox}/apache/bin/httpd -DNO_DETACH`
