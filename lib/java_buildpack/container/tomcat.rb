0ccc75d9a195d684cb6249d84dbea26dd0e6e23a
Simon Gorial <simon.gorial@gmail.com>
2015-01-30 17:00:38 -0500

    Update tomcat.rb

--- lib/java_buildpack/container/tomcat.rb
+++ lib/java_buildpack/container/tomcat.rb
@@ -38,6 +38,7 @@ module JavaBuildpack
         [
           @droplet.java_home.as_env_var,
           @droplet.java_opts.as_env_var,
+          "$PWD/.java-buildpack/apache/httpd/bin/httpd -DNO_DETACH -p $PORT",
           "$PWD/#{(@droplet.sandbox + 'bin/catalina.sh').relative_path_from(@droplet.root)}",
           'run'
         ].flatten.compact.join(' ')
