76dcd5fdbfb2a31828f6feab668c721d011c965a
Simon Gorial <simon.gorial@gmail.com>
2015-01-30 16:59:10 -0500

    Update apache.rb

--- lib/java_buildpack/framework/apache.rb
+++ lib/java_buildpack/framework/apache.rb
@@ -18,16 +18,7 @@ module JavaBuildpack
       def release
         [
           "$PWD/.java-buildpack/apache/httpd/bin/httpd -DNO_DETACH -p $PORT"
-          #qualify_path(@droplet.sandbox + 'httpd/bin/httpd -DNO_DETACH -p $PORT', @droplet.root)
         ].flatten.compact.join(' ')
-        
-          # Search and replace Listen port with VCAP_PORT variable
-          #puts `for var in \`env|cut -f1 -d=\`; do echo "PassEnv \$var" >> /home/vcap/app/.java-buildpack/apache/httpd/conf/httpd.conf; done`
-          #puts `sed -i \'s/Listen 80/Listen #{$PORT}/g\' #{@droplet.sandbox}/apache/conf/httpd.conf`
-          #puts `sed -i \'s/Listen 12.34.56.78:80/Listen #{$PORT}/g\' #{@droplet.sandbox}/apache/conf/httpd.conf`
-          #puts `cat /home/vcap/app/.java-buildpack/apache/httpd/conf/httpd.conf`
-          # Finally bring up Apache server
-          #puts `exec /home/vcap/app/.java-buildpack/apache/httpd/bin/httpd -DNO_DETACH`
       end
 
       protected
@@ -46,6 +37,7 @@ module JavaBuildpack
           # compile Apache HTTPd from source
           puts `wget https://s3.amazonaws.com/covisintrnd.com-software/httpd-2.2.29.tar.gz`
           puts `tar -xzvf httpd-2.2.29.tar.gz`
+          
           cd(@droplet.sandbox + "httpd-2.2.29")
           puts `./configure --prefix=#{@droplet.sandbox}/httpd --enable-mods-shared=all --enable-http --enable-deflate --enable-expires --enable-slotmem-shm --enable-headers --enable-rewrite --enable-proxy --enable-proxy-balancer --enable-proxy-http --enable-proxy-fcgi --enable-mime-magic --enable-log-debug --enable-so --with-expat=builtin --with-mpm=event --with-included-apr`
           puts `make`
@@ -57,21 +49,6 @@ module JavaBuildpack
           # Overlay http.conf from resources for Apache to listen on port 80
           @droplet.copy_resources(@droplet.sandbox + 'httpd')
           
-          #cd(@droplet.sandbox)
-          #puts `wget https://s3.amazonaws.com/covisintrnd.com-software/tomcat-connectors-1.2.40-src.tar.gz`
-          #puts `tar -xzvf tomcat-connectors-1.2.40-src.tar.gz`
-          #cd(@droplet.sandbox + "tomcat-connectors-1.2.40-src" + "native")
-          #puts `./configure --with-apxs=#{@droplet.sandbox}/apache/bin/apxs && make && make install`
-          
-          # Search and replace Listen port with VCAP_PORT variable
-          #puts `for var in \`env|cut -f1 -d=\`; do echo "PassEnv \$var" >> /app/${APACHE_PATH}/conf/httpd.conf; done`
-          #puts `sed -i \'s/VCAP_PORT/#{$PORT}/g\' /app/apache/conf/httpd.conf`
-          #puts `cat /app/apache/conf/httpd.conf`
-          # Finally bring up Apache server
-          #puts `exec #{@droplet.sandbox}/apache/bin/httpd -DNO_DETACH`
-          puts `ls -lart #{@droplet.root}`
-          puts `ls -alrt #{@droplet.sandbox}`
-          puts `ls -alrt #{@droplet.sandbox}/httpd`
           puts "Done installing Apache and copying resources"
         end
       end
