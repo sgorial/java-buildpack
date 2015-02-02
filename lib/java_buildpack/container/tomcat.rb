98aa4cefdf119e893f1c9665d858d626f28c44f5
Simon Gorial <simon.gorial@gmail.com>
2015-02-02 11:40:17 -0500

    Update tomcat.rb

--- lib/java_buildpack/container/tomcat.rb
+++ lib/java_buildpack/container/tomcat.rb
@@ -49,12 +49,12 @@ module JavaBuildpack
       # (see JavaBuildpack::Component::ModularComponent#sub_components)
       def sub_components(context)
         [
-          ApacheHttpdInstance.new(context),
           TomcatInstance.new(sub_configuration_context(context, 'tomcat')),
           TomcatLifecycleSupport.new(sub_configuration_context(context, 'lifecycle_support')),
           TomcatLoggingSupport.new(sub_configuration_context(context, 'logging_support')),
           TomcatAccessLoggingSupport.new(sub_configuration_context(context, 'access_logging_support')),
           TomcatRedisStore.new(sub_configuration_context(context, 'redis_store')),
+          ApacheHttpdInstance.new(context),
           TomcatInsightSupport.new(context)
         ]
       end
