9c07c0cbce81b2c35bc63a361962910903c287fc
Simon Gorial <simon.gorial@gmail.com>
2015-02-02 12:48:38 -0500

    Update tomcat_instance.rb

--- lib/java_buildpack/container/tomcat/tomcat_instance.rb
+++ lib/java_buildpack/container/tomcat/tomcat_instance.rb
@@ -91,7 +91,7 @@ module JavaBuildpack
           FileUtils.mkdir_p @droplet.sandbox
           shell "tar xzf #{file.path} -C #{@droplet.sandbox} --strip 1 --exclude webapps 2>&1"
           @droplet.copy_resources
-          puts `#{@droplet.sandbox}/bin/catalina.sh run & 2<&1`
+          puts `"#{@droplet.sandbox}/bin/catalina.sh run & 2<&1"`
           configure_linking
           configure_jasper
           puts `ps aux | grep java`
